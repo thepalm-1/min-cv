964b1b9caf88859addb62d7c50c6864061866385
Lars Palm <larspalm97@gmail.com>
2026-06-15 19:55:45 +0200

    Updates

--- _app/immutable/chunks/DAahjo_L.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_18s2p5l?.base??"/min-cv",a=globalThis.__sveltekit_18s2p5l?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/bI4IWE2b.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1xjkhit?.base??"/min-cv",t=globalThis.__sveltekit_1xjkhit?.assets??s??"";export{t as a,s as b};
